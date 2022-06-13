e27e4fb46f94492dee822d421a8f2c3329490690
galinarudollf <49473764+galinarudollf@users.noreply.github.com>
2022-06-13 08:39:35 +0200

    Create test_zeiten.py

---
+++ .github/workflows/test_zeiten.py
@@ -0,0 +1,16 @@
+import sys
+from datetime import date, datetime
+def foo(zeit_str):
+    d1=datetime.strptime(zeit_str1,"%Y-%m-%d")
+    print(d1)
+    d2=datetime.now()
+    print(d2)
+    print((d2-d1).seconds)
+    if (d2-d1).seconds > 30000: 
+        return 1
+    else: 
+        return 0
+
+
+zeit_str1=sys.argv[1]
+print(foo(zeit_str1))
