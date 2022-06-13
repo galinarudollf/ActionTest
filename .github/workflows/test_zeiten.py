f1f8fe7438ad48c2edf3127389d74110a5ec02d9
galinarudollf <49473764+galinarudollf@users.noreply.github.com>
2022-06-13 09:42:06 +0200

    Update test_zeiten.py

--- .github/workflows/test_zeiten.py
+++ .github/workflows/test_zeiten.py
@@ -1,17 +1,15 @@
 import sys
-from datetime import date, datetime
+from datetime import datetime
 def foo(zeit_str):
     d1=datetime.strptime(zeit_str1,"%Y-%m-%d")
 #    print(d1)
     d2=datetime.now()
 #    print(d2)
     print((d2-d1).seconds)
-    if (d2-d1).seconds > 30000: 
+    if (d2-d1).seconds > 7*24*60*60: 
         return 1
     else: 
         return 0
 
-
 zeit_str1=sys.argv[1]
 print(foo(zeit_str1))
-# return foo(zeit_str1)
