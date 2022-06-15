83aabc3ecfa2e803c31cefd3e349a1dabbd10002
galinarudollf <49473764+galinarudollf@users.noreply.github.com>
2022-06-15 16:16:32 +0200

    Update test_zeiten.py

--- .github/workflows/test_zeiten.py
+++ .github/workflows/test_zeiten.py
@@ -1,17 +1,32 @@
 import sys
-from datetime import datetime
-def foo(zeit_str):
-    d1=datetime.strptime(zeit_str1,"%Y-%m-%d")
+from datetime import datetime, timedelta
+def foo1(zeit_str):
+    d1=datetime.strptime(zeit_str,"%Y-%m-%d")
 #    print(d1)
     d2=datetime.now()
 #    print(d2)
 #    print((d2-d1).seconds)
 # longer than a week?
-#    if (d2-d1).seconds > 7*24*60*60: 
-#        return '1'
-#    else: 
-#        return '0'
-    return '0'
+    if (d2-d1).seconds > 7*24*60*60: 
+        return '1'
+    else: 
+        return '0'
+
+def foo2(zeit_str):
+    epoch = datetime.utcfromtimestamp(0)
+    today=(datetime.now()-epoch).total_seconds()
+#    print(today)
+    d=datetime.strptime(zeit_str,"%Y-%m-%d")+timedelta(days=7)
+#    print(d)
+    firstcommit=(d-epoch).total_seconds()
+#    print(firstcommit)
+    tomorrow=(datetime.now()+timedelta(days=1)-epoch).total_seconds()
+#    print(tomorrow)
+
+    if today < firstcommit and firstcommit <= tomorrow: 
+        return '1'
+    else: 
+        return '0'
 
 zeit_str1=sys.argv[1]
-print(foo(zeit_str1))
+print(foo2(zeit_str1))
